7e58b1651951f9d39eab387994859f3b923187a8
Carolina <krito_j_g@utp.edu.co>
2017-08-21 11:52:36 -0500

    Separates SafeQueue and client files

--- player/client.cpp
+++ player/client.cpp
@@ -4,66 +4,12 @@
 #include <fstream>
 #include <SFML/Audio.hpp>
 #include <thread>
-#include <queue>
-#include <mutex>
-#include <condition_variable>
+#include "safeQueue.h"
 
 using namespace std;
 using namespace sf;
 using namespace zmqpp;
 
-#ifndef SAFE_QUEUE
-#define SAFE_QUEUE
-
-
-// A threadsafe-queue.
-template <class T>
-class SafeQueue
-{
-public:
-  SafeQueue(void)
-    : q()
-    , m()
-    , c()
-  {}
-
-  ~SafeQueue(void)
-  {}
-
-  // Add an element to the queue.
-  void enqueue(T t)
-  {
-    std::lock_guard<std::mutex> lock(m);
-    q.push(t);
-    c.notify_one();
-  }
-
-  // Get the "front"-element.
-  // If the queue is empty, wait till a element is avaiable.
-  T dequeue(void)
-  {
-    std::unique_lock<std::mutex> lock(m);
-    while(q.empty())
-    {
-      // release lock as long as the wait and reaquire it afterwards.
-      c.wait(lock);
-    }
-    T val = q.front();
-    q.pop();
-    return val;
-  }
-
-  bool isEmpty(void)
-  {
-    return q.empty();
-  }
-
-private:
-  std::queue<T> q;
-  mutable std::mutex m;
-  std::condition_variable c;
-};
-#endif
 
 void messageToFile(const message& msg, const string& fileName){
 	const void *data;
@@ -90,8 +36,6 @@ void songManager(Music *music, SafeQueue<string> &playList, bool &stop) {
     n >> result;
 
     if (result == "file") {
-      cout << "ENTRÓ" << endl;
-      cout << "nextSong : " << nextSong << endl;
 			messageToFile(n, nextSong + ".ogg");
       music->openFromFile(nextSong + ".ogg");
       music->play();
